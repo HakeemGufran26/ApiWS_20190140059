2520b282c2123d8425f99ff07e616b44540e5cdd
Hakeem Gufran <hakeemguran26@gmail.com>
2022-01-05 20:18:53 +0700

    database dan table

---
+++ src/main/java/com/tugasakhirHakeem/tugasakhirHakeem/TugasakhirHakeemApplication.java
@@ -0,0 +1,13 @@
+package com.tugasakhirHakeem.tugasakhirHakeem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasakhirHakeemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasakhirHakeemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugasakhirHakeem/tugasakhirHakeem/TugasakhirHakeemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugasakhirHakeem.tugasakhirHakeem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasakhirHakeemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
